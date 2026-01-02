de89da8113beb30580e746f9ad2b0b01db089aa4
Herbert Emidio <herbertemidio2017@gmail.com>
2026-01-02 17:40:30 -0300

    criando banco modelo relacional

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RastreamentoController;
 use Illuminate\Support\Facades\Route;
 
 
 
 //Controller
 route::get('/', HomeController::class);
+//RastreamantoController
+route::get('/rastreamento',RastreamentoController::class);
