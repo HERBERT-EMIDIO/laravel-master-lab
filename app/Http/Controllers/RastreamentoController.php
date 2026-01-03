d3305dd17cc9bd57911ff01c0e7d23984d3122fa
Herbert Emidio <herbertemidio2017@gmail.com>
2026-01-03 11:28:14 -0300

    metodos

--- app/Http/Controllers/RastreamentoController.php
+++ app/Http/Controllers/RastreamentoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Frete;
 use Illuminate\Http\Request;
 
 class RastreamentoController extends Controller
@@ -12,6 +13,13 @@ class RastreamentoController extends Controller
      */
     public function __invoke(Request $request)
     {
-       return view('frete.rastreamento');
+
+        $frete = Frete::where('codigo_rastreio','TESTE123')->frist();
+
+
+
+       return view('frete.rastreamento',[
+        'frete'=>$frete
+       ]);
     }
 }
